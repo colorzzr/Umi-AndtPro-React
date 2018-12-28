ddb88965a81f2512dfa33657367344eb791f0865
colorzzr <colorzzr@gmai.com>
2018-12-28 15:30:29 -0500

    successfully connect the front-end and backend continue for building all functions'

--- src/pages/test/test.js
+++ src/pages/test/test.js
@@ -23,44 +23,108 @@ import io from 'socket.io-client';
 //   console.log('message was received', data)
 // });
 
+function objDeepCopy(source) {
+  // check it is the array or object
+  const sourceCopy = source instanceof Array ? [] : {};
+
+  const keys = Object.keys(source);
+
+  for (let i = 0; i < keys.length; i += 1) {
+    // eslint-disable-line no-use-before-define
+    if (source != null) {
+      // recursively check the obj in array
+      sourceCopy[keys[i]] =
+        typeof source[keys[i]] === 'object' ? objDeepCopy(source[keys[i]]) : source[keys[i]];
+    }
+  }
+  return sourceCopy;
+}
+
 class TunelGame extends PureComponent {
   constructor(props) {
     super(props);
     const socket = io('http://localhost:3000');
     // remember the client side still need event to detect the response
-    socket.on('connect', () => {
-      // corrosponding with(event_name, arg1, arg2, callback)
-      socket.emit('hello', 'init', 'woot', data => {
-        console.log(data);
-      });
+    // socket.on('connect', () => {
+    // corrosponding with(event_name, arg1, arg2, callback)
+    socket.emit('hello', 'init', 'woot', data => {
+      console.log(data);
     });
-
-    //   var socket = io(); // TIP: io() with no args does auto-discovery
-    // socket.on('connect', function () { // TIP: you can avoid listening on `connect` and listen on events directly too!
-    //   socket.emit('ferret', 'tobi', 'woot', function (data) { // args are sent in order to acknowledgement function
-    //     console.log(data); // data will be 'tobi says woot'
-    //   });
     // });
 
-    this.send = this.send.bind(this);
+    this.defaultColorLogin = this.defaultColorLogin.bind(this);
+    this.messageSend = this.messageSend.bind(this);
 
     this.state = {
       socket,
+      message: [],
+      data: {
+        protocalCode: -1,
+      },
     };
+
+    // taking the thread for broadcast
+    socket.on('broadcast', returnMsg => {
+      const { message } = this.state;
+      console.log('boardcast!');
+      const { source, data } = returnMsg;
+
+      // deep copy the array for the state change
+      const temp = objDeepCopy(message);
+      temp.push(`${source}:${data}`);
+      console.log(temp);
+      this.setState({
+        message: temp,
+        data: returnMsg,
+      });
+    });
   }
 
-  send() {
+  defaultColorLogin() {
     const { socket } = this.state;
-    socket.emit('hello', 'test', 'woot', data => {
+    socket.emit('login', 'color', '1111', data => {
       console.log(data);
+      this.setState({
+        data,
+      });
+    });
+  }
+
+  messageSend() {
+    const { socket, message } = this.state;
+    socket.emit('send', 'color', 'test message', returnMsg => {
+      console.log(returnMsg);
+      const { source, data } = returnMsg;
+
+      // deep copy the array for the state change
+      const temp = objDeepCopy(message);
+      temp.push(`${source}:${data}`);
+      console.log(temp);
+      this.setState({
+        message: temp,
+        data: returnMsg,
+      });
     });
   }
 
   render() {
+    const { data, message } = this.state;
+    const { protocalCode } = data;
+    const msgList = [];
+    console.log(message.length);
+    for (let i = 0; i < message.length; i += 1) {
+      msgList.push(<li key={i}>{message[i]}</li>);
+    }
     return (
       <div>
         <h1>test Page</h1>
-        <Button onClick={this.send}> send! </Button>
+        <h1> current protocal test {protocalCode}</h1>
+        <div>
+          <ul>{msgList}</ul>
+        </div>
+        <Button onClick={this.defaultColorLogin}> default color login </Button>
+        <br />
+        <Button onClick={this.messageSend}> message send </Button>
       </div>
     );
   }
